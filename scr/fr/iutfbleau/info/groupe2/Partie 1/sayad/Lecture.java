a4998096065daa91ec45293e8ee5a2d8279790b9
Youcef <youcef.sayad@epita.fr>
2022-12-18 20:39:13 +0100

    test lecture.java

--- scr/fr/iutfbleau/info/groupe2/Partie 1/sayad/Lecture.java
+++ scr/fr/iutfbleau/info/groupe2/Partie 1/sayad/Lecture.java
@@ -1,31 +1,30 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Lecture{
-    public Lecture(){}
+    private String path;
+    private String output;
 
-    public static void main(String[] args) {
-        StringBuilder output = new StringBuilder();
-        
-        try {
+    public Lecture(String path){
+        this.path = path;
+    }
+
+    public static void main(String[] args) throws Exception {
             URL json = new URL(args[0]);
             InputStream input = json.openStream();
+            BufferedInputStream reader = new BufferedInputStream(input);
             
-            while (input.available() > 0) {
-                output.append(input.read());
+            StringBuilder output = new StringBuilder();
+            while (reader.available() > 0) {
+                output.append((char)reader.read());
             }
 
             System.out.println(output.toString());
 
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        
+            reader.close();    
     }
 }
